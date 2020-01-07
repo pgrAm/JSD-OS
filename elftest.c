50a1fc52b38de561818ade1ca86fe8d36cfcebee
Jake Del Mastro <13jsdm@queensu.ca>
2020-01-07 18:19:45 -0500

    demo some new graphics features

--- elftest.c
+++ elftest.c
@@ -7,32 +7,46 @@ int _start(void)
 {
 	puts("testing\n");
 
-	printf("SS=%X\n", getSS());
-	file_stream* f = open("test.elf", 0);
-	
-	if(f == NULL)
+	do
 	{
-		puts("cannot open file\n");
-		return 0;
-	}
-	
-	char buf[5];
-	
-	read(buf, 4, f);
-	
-	close(f);
-	
-	//spawn_process("test.elf", WAIT_FOR_PROCESS);
+		set_video_mode(80, 50, VIDEO_TEXT_MODE);
+		video_clear();
+		for (int i = 0; i < 256; i++)
+		{
+			putchar(i);
+		}
+		wait_and_getkey();
 
-	buf[4] = 0;
+		set_video_mode(80, 25, VIDEO_TEXT_MODE);
+		video_clear();
+		for (int i = 0; i < 256; i++)
+		{
+			putchar(i);
+		}
+	}
+	while(wait_and_getkey() != 'x');
+
+	printf("SS=%X\n", getSS());
+
+	set_video_mode(320, 200, VIDEO_8BPP);
+
+	uint8_t* mem = map_video_memory();
+	for (int i = 0; i < 100; i++)
+	{
+		memset(mem + i * 2 * 320, i, 320);
+		memset(mem + (i * 2 + 1) * 320, 0, 320);
+	}
+	//spawn_process("test.elf", WAIT_FOR_PROCESS);
 	
-	//while(1)
+	//*(volatile uint8_t*)0 = 1;
+
+	wait_and_getkey();
 	//{
 	//	//puts(buf);
 	//	//puts("\n");
 	//	//puts("hi\n\n");
 	//}
-	
+	set_video_mode(90, 60, VIDEO_TEXT_MODE);
 	exit(42);
 	
 	return 42;
