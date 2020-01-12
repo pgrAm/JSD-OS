d975ea07cc421addfcea56d6473bf58e94726bcd
pgrAm <chuckjonesgame@gmail.com>
2020-01-11 22:07:41 -0500

    use inline asm for port i/o

--- drivers/portio.h
+++ drivers/portio.h
@@ -3,7 +3,19 @@
 
 #include <stdint.h>
 
-uint8_t inb(uint16_t);
-void outb(uint16_t, uint8_t);
+static inline uint8_t inb(uint16_t port)
+{
+	uint8_t retval;
+	__asm__ volatile ("inb %1, %0"
+		: "=a"(retval)
+		: "Nd"(port));
+	return retval;
+}
 
+static inline void outb(uint16_t port, uint8_t val)
+{
+	__asm__ volatile ("outb %0, %1" 
+		: 
+		: "a"(val), "Nd"(port));
+}
 #endif
