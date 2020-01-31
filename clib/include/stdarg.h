2d47cec62ab633243482c02543f939e367ad9337
pgrAm <chuckjonesgame@gmail.com>
2020-01-31 00:55:41 -0500

    fix stdarg to use compiler builtins

--- clib/include/stdarg.h
+++ clib/include/stdarg.h
@@ -1,24 +1,9 @@
 #ifndef STDARG_H
 #define STDARG_H
-typedef unsigned char *va_list;
-
-/* width of stack == width of int */
-#define	STACKITEM	int
-
-/* round up width of objects pushed on stack. The expression before the
-& ensures that we get 0 for objects of size 0. */
-#define	VA_SIZE(TYPE)					\
-	((sizeof(TYPE) + sizeof(STACKITEM) - 1)	\
-		& ~(sizeof(STACKITEM) - 1))
-
-/* &(LASTARG) points to the LEFTMOST argument of the function call
-(before the ...) */
-#define	va_start(AP, LASTARG)	\
-	(AP=((va_list)&(LASTARG) + VA_SIZE(LASTARG)))
-
-#define va_end(AP)	/* nothing */
-
-#define va_arg(AP, TYPE)	\
-	(AP += VA_SIZE(TYPE), *((TYPE *)(AP - VA_SIZE(TYPE))))
+typedef __builtin_va_list va_list;
 	
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_end(v) __builtin_va_end(v)
+#define va_arg(v,l) __builtin_va_arg(v,l)
+
 #endif
